74dbb4fc97ac60d7b86705d63176d01625279623
Nick Elovsky <echonok@gmail.com>
2022-07-09 12:50:40 +0200

    init main files

---
+++ client/src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import GlobalStyles from './styles';
+import Pages from './pages';
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+
+const client = new ApolloClient({
+    uri: 'http://localhost:4000',
+    cache: new InMemoryCache()
+});
+ReactDOM.render(
+    <ApolloProvider client={client}>
+        <GlobalStyles/>
+        <Pages/>
+    </ApolloProvider>,
+    document.getElementById('root')
+);
